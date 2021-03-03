6cd446a5be939bbc0dd70852bb4e65b738eb0b43
jeyansaran620 <jeyansaran620@gmail.com>
2021-03-03 21:35:24 +0530

    adding head function

---
+++ src/head.spec.js
@@ -0,0 +1,13 @@
+const head = require('./head');
+
+describe('Head', () => {
+
+    it('Head of [1,2,3,4] is 1', () => {
+        expect(head([1,2,3,4])).toEqual(1);
+    });
+
+    it('Head of [] is null', () => {
+        expect(head([])).toEqual(undefined);
+    });
+
+})
