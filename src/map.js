d8280ea94fd98ef7a74d367ec31d10f4d527405e
jeyansaran620 <jeyansaran620@gmail.com>
2021-03-03 22:39:46 +0530

    adding map function

--- src/map.js
+++ src/map.js
@@ -1,4 +1,9 @@
-const add = (first,second) => {
-    return first + second;   
+const map = (array,func) => {
+    if(array.length<1)
+        return array;
+    let new_array = [];
+    for(i=0;i<array.length;i++)
+        new_array[i]=func(array[i]);
+    return new_array;   
 }
-module.exports = add;
+module.exports = map;
--- src/map.spec.js
+++ src/map.spec.js
@@ -1,13 +1,22 @@
-const add = require('./add');
+const map = require('./map');
 
-describe('Add', () => {
+describe('Map', () => {
 
-    it('Sum of 0 and 0 is 0', () => {
-        expect(add(0, 0)).toEqual(0);
+    it('Map of [] and cube and  is []', () => {
+        
+        expect(map([], element=>math.pow(element,3))).toEqual([]);
     });
 
-    it('Sum of 1 and 0 is 1', () => {
-        expect(add(1, 0)).toEqual(1);
+    it('Map of [1,2,3], identity and  is [1,2,3]', () => {
+        expect(map([1,2,3], element=>element*1)).toEqual([1,2,3]);
+    });
+
+    it('Map of [1,2,3] and cube and  is [1,8,27]', () => {
+        expect(map([1,2,3], element=> Math.pow(element,3))).toEqual([1,8,27]);
+    });
+
+    it('Map of [a{x : 10}] and someObject => someObject.x + 1 and  is [11]', () => {
+        expect(   map([a={x : 10}], someObject => someObject.x + 1)).toEqual([11]);
     });
 
 })
